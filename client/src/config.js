f181a8f6454491dbb4ed578996959fbd63bf24c9
Praneeth2k <bpraneeth2000@gmail.com>
2022-06-06 15:27:27 +0530

    add chainlink feature for matic price

--- client/src/config.js
+++ client/src/config.js
@@ -5,6 +5,7 @@
 export const nftaddress = "0xB0e9e4dB0Aeb567dCf899C8d399c29634bB576Db"
 export const memeitaddress = "0xD8011EAD62bADd7C74fA4A13c105bb641494AB76"
 export const tokenaddress = "0x94E723b6fE116E20752D20df549f12f58De45D23"
+export const maticaddress = "0x3d6c5eE1fC4F8C3ED4378fcc688FE50f3FF3F1c6"
 
 // export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 // export const memeitaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
